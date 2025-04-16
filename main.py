9ba7d925d62cfc5303e5988dea0718824e82ad3f
Dark Adam xp <dissatechyt@gmail.com>
2025-04-16 10:29:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,8 +27,8 @@ def init(gmail, password, titulo, body):
 def main():
 	while True:
 		system('cls||clear')
-		op = input('''\n\n%s- %sDiscord%s  : kine azul#3189
-- %sYoutube%s  : https://youtube.com
+		op = input('''\n%s- %sDiscord%s  : kine azul#3189
+- %sGithub%s  : https://github.com/Adamxking404
 - %sTelegram%s : @Adamxking\n\n[%s 1 %s] %s|%s Ban number\n[%s 2 %s] %s|%s Unban number\n[%s 0 %s] %s|%s Exit\n\n%s===> %s'''%(logo,Azul,Branco,Azul,Branco,Azul,Branco,Azul,Branco,Azul,Branco,Azul,Branco,Azul,Branco,Azul,Branco,Azul,Branco,Azul,Branco))
 		if op in ['0']:
 			break
@@ -58,7 +58,7 @@ logo='''%s
 |  _  | / /\ \ |  _  | |\_/| |
 | | | |/ /__\ \| | | | |   | |
 |_| |_/________\_| |_|_|   |_|
-%sA WHATSAPP NUMBER BAN TOOL BY DARK ADAM\n%s•%s Turn on the less secure apps option.\n'''%(Azul,Branco,Azul,Branco)
+%sA WHATSAPP NUMBER BAN TOOL BY DARK ADAM\n%s•%s Turn on the less secure apps option.\n\n'''%(Azul,Branco,Azul,Branco)
 ##################
 if __name__ == '__main__':
 	main()
